6f61b2245d715a08d11b7fc80ec5120e7753ffa0
Neha Saxena <61897573+Neha-Saxena15@users.noreply.github.com>
2021-07-09 21:03:01 +0530

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter the number of elements in the array :";
+	cin>>n;
+int arr[n];
+cout<<endl<<"enter the elements of the array:"<<endl;
+for(int i=0;i<n;i++)
+      cin>>arr[i];
+	cout<<"before sorting the array is:"<<" ";
+ for(int i=0;i<n;i++)
+      	cout<<arr[i]<<" ";
+	       
+	  cout<<endl<<"after sorting the array is:"<<" "<<endl;  
+	  
+	  //Bubble Sort
+	  for(int i=0;i<n;i++)
+	    {
+	    	for(int j=0;j<n;j++)
+	    	  {
+	    	  	if(arr[j]<arr[i])
+				  	{
+				  	int temp;
+					 temp = arr[i];
+					 arr[i] = arr[j];
+					 arr[j] = temp; 	
+					}    	  	
+			  }
+		}
+for(int i=0;i<n;i++)
+      	cout<<arr[i]<<" ";
+	       
+	   return 0;    	
+}
